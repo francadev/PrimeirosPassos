e1ab3e3ada5d530a97c5ab89c3d1449d7397c98c
Rafael <francadev@hotmail.com>
2022-02-24 18:53:54 -0300

    Instanciação de Objetos

--- Aula04/Aula04/Program.cs
+++ Aula04/Aula04/Program.cs
@@ -10,7 +10,28 @@ namespace Aula04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fim da execução!");
+            Tempo tempo1;// variavel capaz de acessar o objeto
+            tempo1 = new Tempo(); // new cria o objeto na memoria e devolve uma referencia para ele
+
+            tempo1.hora = 9;
+            tempo1.minuto = 42;
+            tempo1.segundo = 28;
+
+            Console.WriteLine("tempo.hora    {0:D2}", tempo1.hora);
+            Console.WriteLine("tempo.minuto  {0:D2}", tempo1.minuto);
+            Console.WriteLine("tempo.segundo {0:D2}", tempo1.segundo);
+
+            Tempo tempo2 = new Tempo();
+
+            tempo2.hora = 23;
+            tempo2.minuto = 5;
+            tempo2.segundo = 30;
+
+            Console.WriteLine("\ntempo.hora    {0:D2}", tempo2.hora);
+            Console.WriteLine("tempo.minuto  {0:D2}", tempo2.minuto);
+            Console.WriteLine("tempo.segundo {0:D2}", tempo2.segundo);
+
+            Console.WriteLine("\nFim da execução!");
         }
     }
 }
